de1af0e378e4bb407fbcba425fc4ccba73897bcf
kozok123 <kozak02947@gmail.com>
2026-09-04 10:22:45 +0200

    losowanie i tworzenie listy

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,27 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] wylosowanaTablica = new int[6];
+        ArrayList<Integer>listaWylosowana = new ArrayList<>();
         Random random = new Random();
-        //losowanie bez powtorzen
+        //losowanie listy bez powtorzen
         int liczba ;
-        boolean czyZawiera;
-        for (int i = 0; i < wylosowanaTablica.length; i++) {
-            liczba = random.nextInt(1,50);
-            for (int j = 0; j<= i; j++) {
-                if (wylosowanaTablica[j] == liczba) {
-                    czyZawiera = true;
-                    break;
-                }
+        while (listaWylosowana.size()<6) {
+            liczba = random.nextInt(1, 50);
+            if (listaWylosowana.contains(liczba)){
+                continue;
             }
-            if (!czyZawiera) {
-                wylosowanaTablica[i]=liczba;
-            }
-            czyZawiera = false;
+            listaWylosowana.add(liczba);
+
         }
         //wypisywanie tablicy
-        System.out.println("tablica losowana:");
-        for (int elementTablicy:wylosowanaTablica) {
-            System.out.println(elementTablicy);
-        }
+        System.out.println("Wylosowana lista::");
+
+        System.out.println(listaWylosowana);
+
 
     }
 }
