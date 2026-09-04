36431a1d5d4f99e29cadaca35406c20d07e064ac
kozok123 <kozak02947@gmail.com>
2026-09-04 10:15:19 +0200

    losowanie tablicy bez powtorzen

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,27 @@ public class Main {
     public static void main(String[] args) {
         int[] wylosowanaTablica = new int[6];
         Random random = new Random();
+        //losowanie bez powtorzen
+        int liczba ;
+        boolean czyZawiera;
         for (int i = 0; i < wylosowanaTablica.length; i++) {
-            wylosowanaTablica[i] = random.nextInt(1,50);
+            liczba = random.nextInt(1,50);
+            for (int j = 0; j<= i; j++) {
+                if (wylosowanaTablica[j] == liczba) {
+                    czyZawiera = true;
+                    break;
+                }
+            }
+            if (!czyZawiera) {
+                wylosowanaTablica[i]=liczba;
+            }
+            czyZawiera = false;
         }
         //wypisywanie tablicy
         System.out.println("tablica losowana:");
         for (int elementTablicy:wylosowanaTablica) {
             System.out.println(elementTablicy);
         }
+
     }
 }
